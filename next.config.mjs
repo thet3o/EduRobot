7620c1550d2cea41524bac1616546f12fef231d6
Matteo Vacalebri <thet3o@Matteos-MacBook-Air.local>
2024-08-01 11:20:29 +0200

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@ import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
-  cacheOnFrontEndNav: true,
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   reloadOnOnline: true,
